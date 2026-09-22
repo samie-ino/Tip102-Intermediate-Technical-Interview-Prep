a3723f5743807b57869e4cdc7e35b22156600431
Samone Cook <samone.a.cook-1@ou.edu>
2026-09-22 18:47:01 -0500

    Add functions for artist lineup and ticket sales
    Implemented functions to map artists to set times and calculate total ticket sales.

--- Week 2/problem_set1.py
+++ Week 2/problem_set1.py
@@ -1,1 +1,52 @@
+"""
+Problem 1
+UNDERSTAND:
+Input: 2 lists of strings: artists and set_times
+Output: dictionary mapping artist names to set time
+Edge Cases:
+- 
+
+PLAN:
+- Have an empty dictionary to store the mapping
+- for loop through in the range of the length of the artists list
+- for each index, add the artist name as the key and its set time
+- Then return the dictionary
+
+IMPLEMENT: (below)
+"""
+
+
+def lineup(artists, set_times):
+    lineup = {}
+
+    for i in range(len(artists)):
+        lineup[artists[i]] = set_times[i]
+    return lineup
+
+
+"""
+Problem 2
+UNDERSTAND:
+Input: dictionary mapping ticket type to number of tickets sold
+Output: integer representing all tickets sold
+Edge Cases:
+- if tickets sold is negative -> 0
+- empty dictionary -> return 0
+
+PLAN:
+- create a variable count to store number of tickets
+- loop through the keys in ticket_sales
+- for each ticket type, add that total to count
+- return count
+
+IMPLEMENT: (below)
+"""
+
+
+def total_sales(ticket_sales):
+    count = 0
+
+    for value in ticket_sales.values():
+        count += value
+    return count
 
